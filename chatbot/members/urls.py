2725df7b590141d8e9f748ce4d501b8a4b6735eb
tavis <tavishbalyan@gmail.com>
2025-03-02 21:54:07 +1100

    Update .gitignore, remove gemini api

--- chatbot/members/urls.py
+++ chatbot/members/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('chatbot/', views.chatbot, name='chatbot'),
     path('query/', views.chatbot_query, name='chatbot_query'),
-    path('get_key/', views.get_key, name='get_key'),
 ]
